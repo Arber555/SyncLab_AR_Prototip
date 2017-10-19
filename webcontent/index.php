49b62104d10e1cc080e12fe60a9845788494b0b5
Arber555 <arber555.am@gmail.com>
2017-10-19 13:22:52 +0200

    objektet me dal u bo

--- webcontent/index.php
+++ webcontent/index.php
@@ -4,42 +4,61 @@
 <script src="../ARjs/aframe-ar.js"></script>
 <body style='margin : 0px; overflow: hidden;'>
     <div style='position: absolute; top: 10px; width:100%; text-align: center; z-index: 1;'>
-        <input type="button" value="Remove" onclick="trupat();" />
+        <input type="button" value="Sphere" onclick="trupat('reth');" />
+        <input type="button" value="Box" onclick="trupat('kub');" />
     </div>
-  <a-scene embedded arjs='trackingMethod: best;'>
-        <a-marker preset='hiro'>
+    <a-scene embedded arjs='trackingMethod: best;' id="scene">
+        <a-marker preset='hiro' id="marker">
             <!-- here define the content to display on top of the marker -->
-            <a-box position='0 0.5 0' material='color: red; opacity: 0.5;'></a-box>
+           <!-- <a-box position='0 0.5 0' material='color: red; opacity: 0.5;' id="object"></a-box>-->
             <!--<a-sphere position='0 0.5 0' material='color: red; opacity: 0.5;'s></a-sphere>-->
+
         </a-marker>
+
+        <!-- define your gltf asset -->
+            <a-assets>
+                <a-asset-item id="tree" src="../3D_Models/busterDrone/busterDrone.gltf"></a-asset-item>
+            </a-assets>
+                <!-- use your gltf model -->
+            <a-entity gltf-model="#tree"></a-entity>
+
             <!-- define a simple camera -->
         <a-entity camera></a-entity>
-
-  </a-scene>
+    </a-scene>
   <script>
-        var elements = document.getElementsByTagName('a-marker');
+        //var elements = document.getElementsByTagName('a-marker');
+        var elements = document.getElementById("marker");
         function remove()
         {
             //var elements = document.getElementsByTagName('a-marker');
 
-            while (elements[0]) {
+            /*while (elements[0]) {
                 elements[0].parentNode.removeChild(elements[0]);  //qetu osht gabimi kqyre edhe nihere
-            }
+            }*/
+
+            elements.removeChild(document.getElementById("object"));
         }
 
-        function trupat()
+        function trupat(x)
         {
-            //if(x == "reth")
-            //{
-                //remove();
+            if(x == "reth")
+            {
+                remove();
                 
                 var mycontent = document.createElement("a-sphere");
                 mycontent.setAttribute("position", "0 0.5 0");
                 mycontent.setAttribute("material", "color: red; opacity: 0.5;");
-                //mycontent.appendChild(document.createTextNode("This is a paragraph"));
-                elements[0].appendChild(mycontent);
-                //elements.innerHTML = "<a-sphere position='0 0.5 0' material='color: red; opacity: 0.5;'s></a-sphere>";
-            //}
+                mycontent.setAttribute("id", "object");
+                elements.appendChild(mycontent);
+            } else if(x == "kub") {
+                remove();
+                
+                var mycontent = document.createElement("a-box");
+                mycontent.setAttribute("position", "0 0.5 0");
+                mycontent.setAttribute("material", "color: red; opacity: 0.5;");
+                mycontent.setAttribute("id", "object");
+                elements.appendChild(mycontent);
+            }
         }
 
   </script>
