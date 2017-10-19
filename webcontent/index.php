53746b23c12fb7bd1c64d2a6be08a35f9dbf0e9f
Arber555 <arber555.am@gmail.com>
2017-10-19 10:56:07 +0200

    AR.js tu e bo me shti trupa edhe ma hek

--- webcontent/index.php
+++ webcontent/index.php
@@ -3,13 +3,45 @@
 <!-- include ar.js for A-Frame -->
 <script src="../ARjs/aframe-ar.js"></script>
 <body style='margin : 0px; overflow: hidden;'>
+    <div style='position: absolute; top: 10px; width:100%; text-align: center; z-index: 1;'>
+        <input type="button" value="Remove" onclick="trupat();" />
+    </div>
   <a-scene embedded arjs='trackingMethod: best;'>
         <a-marker preset='hiro'>
             <!-- here define the content to display on top of the marker -->
             <a-box position='0 0.5 0' material='color: red; opacity: 0.5;'></a-box>
+            <!--<a-sphere position='0 0.5 0' material='color: red; opacity: 0.5;'s></a-sphere>-->
         </a-marker>
             <!-- define a simple camera -->
         <a-entity camera></a-entity>
+
   </a-scene>
+  <script>
+        var elements = document.getElementsByTagName('a-marker');
+        function remove()
+        {
+            //var elements = document.getElementsByTagName('a-marker');
+
+            while (elements[0]) {
+                elements[0].parentNode.removeChild(elements[0]);  //qetu osht gabimi kqyre edhe nihere
+            }
+        }
+
+        function trupat()
+        {
+            //if(x == "reth")
+            //{
+                //remove();
+                
+                var mycontent = document.createElement("a-sphere");
+                mycontent.setAttribute("position", "0 0.5 0");
+                mycontent.setAttribute("material", "color: red; opacity: 0.5;");
+                //mycontent.appendChild(document.createTextNode("This is a paragraph"));
+                elements[0].appendChild(mycontent);
+                //elements.innerHTML = "<a-sphere position='0 0.5 0' material='color: red; opacity: 0.5;'s></a-sphere>";
+            //}
+        }
+
+  </script>
 </body>
 </html>
